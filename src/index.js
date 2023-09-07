b45af69190c2fd98f7dbc467d2e50506ac9d66eb
Diana <taaldiana@gmail.com>
2023-09-07 12:33:47 +0300

    eemaldasin database kausta/faili ja panin kõik index.js sisse

---
+++ src/index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+
+const app = express();
+
+// Middleware Configuration
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Database Connection
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'qwerty',
+    database: 'DianaSQL'
+});
+connection.connect();
+
+// Define Routes
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+app.get('/api/articles', (req, res) => {
+    // Fetch articles from the database (if applicable)
+    // Send JSON response
+});
+
+// Start the Server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
+// Error Handling (optional)
+app.use((req, res, next) => {
+    res.status(404).json({ error: 'Not Found' });
+});
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ error: 'Internal Server Error' });
+});
